6525cf2b9060f99320c13e6910f9038387aadc89
Huy Tran <hqtran@wpi.edu>
2016-11-27 23:51:20 -0500

    initialize class

--- src/player/view/PlayerApplication.java
+++ src/player/view/PlayerApplication.java
@@ -1,5 +1,10 @@
 package player.view;
 
 public class PlayerApplication {
-
+	public static void main(String[] args) {
+		SplashScreen splashScreen = new SplashScreen(2);
+		splashScreen.show();
+		
+		// MenuScreen menuScreen = new MenuScreen();
+	}
 }

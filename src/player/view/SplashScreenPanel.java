14456f04e8092d1eba85123258c3dc6bf5a43e10
Huy Tran <hqtran@wpi.edu>
2016-11-28 22:18:53 -0500

    add exception

--- src/player/view/SplashScreenPanel.java
+++ src/player/view/SplashScreenPanel.java
@@ -16,6 +16,7 @@ public class SplashScreenPanel extends JPanel {
 	       try {                
 	          splashScreen = ImageIO.read(new File("/cobalt-lettercraze/Images/SplashScreen.png"));
 	       } catch (IOException ex) {
+	    	   	System.out.println(ex.getMessage());
 	       }
 	    }
 

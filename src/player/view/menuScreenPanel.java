7aac1c5119553b5db9f19172bc4d904ed7863308
Barry Hong <hunghongphugia@gmail.com>
2016-11-28 23:05:19 -0500

    Add Menu Screen, Panel, and Model

---
+++ src/player/view/menuScreenPanel.java
@@ -0,0 +1,14 @@
+package player.view;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class menuScreenPanel extends JPanel{
+
+	JButton[] levelButton = new JButton[15];
+	JButton tutorialButton = new JButton();
+	
+	public void placePanel(){
+		
+	}
+}

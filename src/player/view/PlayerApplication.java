e04a77e69691800a806bf73d379d56f40afd9934
Barry Hong <hunghongphugia@gmail.com>
2016-11-28 20:52:39 -0500

    SplashScreenWindow & PlayerApplication edited

--- src/player/view/PlayerApplication.java
+++ src/player/view/PlayerApplication.java
@@ -2,9 +2,7 @@ package player.view;
 
 public class PlayerApplication {
 	public static void main(String[] args) {
-		SplashScreenWindow splashScreen = new SplashScreenWindow(2);
-		splashScreen.show();
-		
-		// MenuScreen menuScreen = new MenuScreen();
+		SplashScreenWindow splashScreen = new SplashScreenWindow(1000);
+		splashScreen.showSplash();
 	}
 }
--- src/player/view/SplashScreenWindow.java
+++ src/player/view/SplashScreenWindow.java
@@ -1,30 +1,23 @@
 package player.view;
 
-import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.JWindow;
 
-public class SplashScreenWindow extends JWindow {
-	/**
-	 * Dureation of the splash screen
-	 */
+public class SplashScreenWindow extends JWindow{
+
 	private int duration;
 	
-	public SplashScreenWindow(int duration) {
-		this.duration = duration;
+	public SplashScreenWindow(int d) {
+		this.duration = d;
 	}
-	
-	/**
-	 * Initialize splash screen similar to design and show it
-	 */
-	public void show() {
-		JPanel content = (JPanel) getContentPane();
-		setBounds(0, 0, 800, 600);
-		setVisible(true);
-		content.add(new SplashScreenPanel());
-//		 Wait a little while, maybe while loading resources
-		try { Thread.sleep(duration); } catch (Exception e) {}
 
+	public void showSplash() {
+		JPanel content = (JPanel) getContentPane();
+		setBounds(0,0,500,500);
+		setVisible(true);
+		try {
+			Thread.sleep(duration);
+		} catch (Exception e) {}
 		setVisible(false);
 	}
 }

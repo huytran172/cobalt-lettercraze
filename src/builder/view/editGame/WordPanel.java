ec466dc58f993f2152f8b10f5b90ba33cc5ab00f
Khuyen Cao <hakhuyen1997@gmail.com>
2016-11-30 01:15:56 -0500

    builder add game panel

---
+++ src/builder/view/editGame/WordPanel.java
@@ -0,0 +1,14 @@
+package builder.view.editGame;
+
+import java.awt.Color;
+
+import javax.swing.JPanel;
+
+public class WordPanel extends JPanel {
+	
+	WordPanel(){
+		setSize(200,160);
+		setBackground(Color.red);
+	}
+
+}

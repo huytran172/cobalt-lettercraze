df63649d5dc2bfff10f05df71a3fb7a72bbff0cc
quyendinhthuchoang <quyendinhthuchoang@gmail.com>
2016-11-29 21:25:49 -0500

    add info panel

---
+++ src/player/view/game/InfoPanel.java
@@ -0,0 +1,12 @@
+package player.view.game;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+
+public class InfoPanel extends JPanel {
+	
+	JButton highScore;
+	JButton currentScore;
+	JSlider progress;
+}

6714ec16f0c0c3dee4df44f54b440ea8b7774e72
Trang <hadieutrang@gmail.com>
2016-12-11 20:18:05 -0500

    Changed Info Panel

---
+++ src/builder/controller/PreviewController.java
@@ -0,0 +1,33 @@
+package builder.controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JTextField;
+
+import builder.view.BuilderApplication;
+import player.models.*;
+
+
+public class PreviewController implements ActionListener {
+	BuilderApplication application;     /** Application we have control over. */
+	Model model;                 /** Model containing state. */
+	
+	public PreviewController(BuilderApplication app, Model m) {
+		this.application = app;
+		this.model = m;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JTextField tf = application.getFahrenheitField();
+		
+		IMove move = new ModifyValue(model, Integer.valueOf(tf.getText()));
+		
+		// request move
+		if (move.execute()) {
+			UndoManager.instance().recordMove(move);
+			application.refresh();
+		}
+	}
+}

7f609f15b85eb0145bd7a35917df3af15e6a5a13
Huy Tran <hqtran@wpi.edu>
2016-12-14 23:25:38 -0500

    init

--- src/player/view/game/submitbutton/PuzzleSubmitButton.java
+++ src/player/view/game/submitbutton/PuzzleSubmitButton.java
@@ -12,6 +12,17 @@ public class PuzzleSubmitButton extends CustomSubmitButton {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		GamePanel gamePanel = (GamePanel) infoPanel.getParent();
+		word = board.getTempWord();
+		if (word.validWord()) {
+			this.updateScore(word);
+			System.out.println("SCORE " + this.score.getScore());
+			wordPanel.addToWordPanel(word.getWordString());
+			word.clearWord();
+			board.updateBoard();
+			board.fillEmptySquares();
+			gamePanel.getBoardPanel().reputLetter();
+		}
 	}
 
 }

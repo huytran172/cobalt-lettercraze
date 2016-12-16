6835c4a829bc44ec806695bb70114f7cc6b51d62
quyendinhthuchoang <quyendinhthuchoang@gmail.com>
2016-12-15 21:21:03 -0500

    magic

---
+++ src/builder/view/editGame/TestPanel.java
@@ -0,0 +1,18 @@
+package builder.view.editGame;
+
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+
+public class TestPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TestPanel() {
+		
+		JLabel lblHelloWorld = new JLabel("Hello World");
+		add(lblHelloWorld);
+
+	}
+
+}

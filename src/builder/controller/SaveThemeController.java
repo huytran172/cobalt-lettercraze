a9310c9f70e9f2b028fea0cc260d098d25269cf4
Huy Tran <hqtran@wpi.edu>
2016-12-16 05:11:01 -0500

    edit theme

--- src/builder/controller/SaveThemeController.java
+++ src/builder/controller/SaveThemeController.java
@@ -27,14 +27,13 @@ public class SaveThemeController implements ActionListener {
 		this.b = b;
 		this.levelNum = levelNum;
 		this.add = add;
-
 	}
 	
 	/**
 	 * Create a new name for the freshly created level
 	 * @return
 	 */
-int levelName(){
+	int levelName(){
 		
 		File[] files = new File("../cobalt-lettercraze/levelsBuilderMade/").listFiles();
 
@@ -78,13 +77,6 @@ int levelName(){
 		stars.add(star_1);
 		stars.add(star_2);
 		stars.add(star_3);
-		
-		
-		
-//		IMove move = new ModifyValue(model, Integer.valueOf(tf.getText()));
-//		
-		// request move
-
 
 		try {
 			//Create new file
@@ -139,11 +131,11 @@ int levelName(){
 			    //PrintWriter writer = new PrintWriter("Level16", "UTF-8");
 			    
 			    //Add level name
-			    writer.println(String.format("%d", levelName()-1));
+			    writer.println(String.format("%d", levelNum));
 			    
 			    //Maximum number of words
 			    writer.print(maxWords + " ");
-			  //List of words to find
+			  	//List of words to find
 			    for (int i = 0; i < list.length; i++){
 			    	writer.print(list[i] + " ");
 			    }
@@ -151,31 +143,21 @@ int levelName(){
 			    writer.println();
 			    
 			    //Thresholds
-			    for (int j = 0; j <3; j++){
+			    for (int j = 0; j < 3; j++){
 			    	writer.print(stars.get(j) + " ");
 			    }
 			    
 			    writer.println();
-			    
-			    
 			    //High score
 			    writer.println(0);
-			    
 			    //Is complete?
 			    writer.println(0);
-			    
 			    //Board visibility
 			    writer.println(b.toStringForBuilder());
 			    writer.close();
-				
 			}
-		    
-		    
-		    
 		} catch (IOException exception) {
 			exception.printStackTrace();
 		}
-		
-		
 	}
 }
